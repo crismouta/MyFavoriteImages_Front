24f179c23ae3fbb5f730bd8133195a1e939d97bb
crismouta <crismouta@gmail.com>
2023-11-06 19:18:28 +0100

    styles, clean cache and add cache files to gitignore

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,16 @@
-import { Link } from 'react-router-dom';
-import './home.css';
+import { Link } from "react-router-dom";
+import "./home.css";
 
 const Home = () => {
     return (
         <div className="home-background">
-            <Link to='/images' className='link-title'>
-                <div className='home-title-container'>
-                    <h1 className='home-title'>My favorite Images</h1>
+            <Link to="/images" className="link-title">
+                <div className="home-title-container">
+                    <h1 className="home-title">My favorite Images</h1>
                 </div>
             </Link>
-
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
